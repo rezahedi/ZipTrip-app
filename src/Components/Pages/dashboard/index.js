4e30c9a9c00b69a7dc3a07add4fbaa3dc54c5b46
Reza Zahedi <r.zahedi@gmail.com>
2025-04-23 16:47:33 -0700

    Fix: import two new components

--- src/Components/Pages/dashboard/index.js
+++ src/Components/Pages/dashboard/index.js
@@ -2,5 +2,7 @@ import DashboardTheme from "./Theme"
 import MyPlans from "./MyPlans"
 import Bookmarked from "./Bookmarked"
 import CreateNew from "./CreateNew"
+import Done from "./Done"
+import Profile from "./Profile"
 
-export { DashboardTheme, MyPlans, Bookmarked, CreateNew }
+export { DashboardTheme, MyPlans, Bookmarked, CreateNew, Done, Profile }
