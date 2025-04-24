d1c68b6be47cacc99e0ad2c40a80ac50e3361154
Reza Zahedi <r.zahedi@gmail.com>
2025-04-23 22:59:47 -0700

    improve UI for MyPlans component

--- src/Components/Pages/dashboard/MyPlans.jsx
+++ src/Components/Pages/dashboard/MyPlans.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { getMyPlans } from '../../../util/dashboard'
 import PlanCard from '../../Common/PlanCard'
-import { Grid, Button } from "@mui/material"
+import { Box, Grid, Button, Typography } from "@mui/material"
 
 function MyPlans() {
   const [plans, setPlans] = useState([])
@@ -21,20 +21,21 @@ function MyPlans() {
   }
 
   return (
-    <div>
-      <div>
-        <h1>My Plans</h1>
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 2 }}>
+        <Typography variant="h4">
+          My Plans
+        </Typography>
         <Button component={Link} to="/account/create" color="inherit">
           Create New Plan
         </Button>
-      </div>
+      </Box>
       {error && <p>{error}</p>}
       {plans.length > 0 ? (
         <Grid container spacing={3}>
           {plans.map((plan) => (
-            <Grid size={{ xs: 12, sm: 6, md: 4 }} key={plan.planId}>
+            <Grid size={{ xs: 12, sm: 6, md: 4 }} key={plan._id} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
               <PlanCard 
-                key={plan._id}
                 image={plan.images[0]}
                 title={plan.title}
                 rate={plan.rate}
@@ -42,15 +43,17 @@ function MyPlans() {
                 distance={plan.distance}
                 stopCount={plan.stopCount}
               />
-              <Button component={Link} to={`/account/${plan._id}`} color="primary" sx={{ marginTop: 1 }}>Edit</Button>
-              <Button onClick={() => handleRemovePlan(plan._id)} color="error" sx={{ marginLeft: 1, marginTop: 1 }}>Remove</Button>
+              <Box>
+                <Button component={Link} to={`/account/${plan._id}`} sx={{ marginTop: 1 }}>Edit</Button>
+                <Button onClick={() => handleRemovePlan(plan._id)} style={{ backgroundColor: '#f44336', color: 'white' }} sx={{ marginLeft: 1, marginTop: 1 }}>Remove</Button>
+              </Box>
             </Grid>
           ))}
         </Grid>
       ) : (
         <p>No plans found.</p>
       )}
-    </div>
+    </>
   )
 }
 
