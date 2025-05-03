2d6e79775453684265e206dcb0002c33047bce55
Reza Zahedi <r.zahedi@gmail.com>
2025-05-03 01:41:29 -0700

    feat: add timeline-style to stops

--- src/Components/Common/Stops.jsx
+++ src/Components/Common/Stops.jsx
@@ -2,18 +2,57 @@ import React from "react";
 import PropTypes from "prop-types";
 import Stop from "./Stop";
 import { Box, Typography } from "@mui/material";
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineDot,
+  TimelineContent,
+} from "@mui/lab";
+import RoomOutlinedIcon from "@mui/icons-material/RoomOutlined";
 
 const Stops = ({ stops }) => {
   return (
-    <Box sx={{ width: { md: 760, xs: "100%" } }}>
-      <Typography variant="h4">Activities</Typography>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-        <Typography variant="h5">Start Point</Typography>
+    <Box sx={{ mt: 4, width: { md: 760, xs: "100%" } }}>
+      <Typography variant="h4" sx={{ mb: 2 }}>
+        Activities
+      </Typography>
+      <Timeline
+        position="right"
+        sx={{
+          width: "100%",
+          pl: 0,
+          ".MuiTimelineItem-missingOppositeContent:before": { display: "none" },
+          ".MuiTimelineContent-root": { pt: 1.5, pr: 0, pb: 4, pl: 2 },
+          ".MuiTimelineDot-root": { backgroundColor: 'transparent', boxShadow: 'none', color: '#4CAF50', p: 0 },
+        }}
+      >
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineDot><RoomOutlinedIcon sx={{width:30, height:30}} /></TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent><Typography variant="h5">Start Point</Typography></TimelineContent>
+        </TimelineItem>
         {stops.map((stop, index) => (
-          <Stop key={index} {...stop} />
+          <TimelineItem key={index}>
+            <TimelineSeparator>
+              <TimelineDot><RoomOutlinedIcon sx={{width:30, height:30}} /></TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Stop {...stop} />
+            </TimelineContent>
+          </TimelineItem>
         ))}
-        <Typography variant="h5">Finish Point</Typography>
-      </Box>
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineDot><RoomOutlinedIcon sx={{width:30, height:30}} /></TimelineDot>
+          </TimelineSeparator>
+          <TimelineContent><Typography variant="h5">Finish Point</Typography></TimelineContent>
+        </TimelineItem>
+      </Timeline>
     </Box>
   );
 };
