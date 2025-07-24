33f83085a89025cbfdc4d6d52d7abea54ce95208
Reza Zahedi <r.zahedi@gmail.com>
2025-07-24 13:22:21 -0700

    install shadcn ui

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -15,6 +16,11 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
