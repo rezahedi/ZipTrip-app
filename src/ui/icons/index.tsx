a06ed1f943dd9d8454ec20c2996d13dbb8df7633
Reza Zahedi <r.zahedi@gmail.com>
2025-07-29 15:55:00 -0700

    add few svg icons

--- src/ui/icons/index.tsx
+++ src/ui/icons/index.tsx
@@ -1,3 +1,8 @@
-import Menu from "./menu";
+import Menu from "./Menu";
+import Forward from "./Forward";
+import PhotoPlus from "./PhotoPlus";
+import Profile from "./Profile";
+import Setting from "./Setting";
+import Close from "./Close";
 
-export { Menu };
+export { Menu, Forward, PhotoPlus, Profile, Setting, Close };
