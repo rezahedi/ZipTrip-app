4fe440044715ba180a9bb72c618af78f53fa3805
Reza Zahedi <r.zahedi@gmail.com>
2025-07-29 16:49:13 -0700

    add logout icon

--- src/ui/icons/index.tsx
+++ src/ui/icons/index.tsx
@@ -4,5 +4,6 @@ import PhotoPlus from "./PhotoPlus";
 import Profile from "./Profile";
 import Setting from "./Setting";
 import Close from "./Close";
+import Logout from "./Logout";
 
-export { Menu, Forward, PhotoPlus, Profile, Setting, Close };
+export { Menu, Forward, PhotoPlus, Profile, Setting, Close, Logout };
