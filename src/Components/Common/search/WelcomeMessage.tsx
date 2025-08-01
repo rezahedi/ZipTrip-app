ad04f4f07792af831af3712bd6d4964e686df4b5
Reza Zahedi <r.zahedi@gmail.com>
2025-08-01 16:31:05 -0700

    refactor search components

--- src/Components/Common/search/WelcomeMessage.tsx
+++ src/Components/Common/search/WelcomeMessage.tsx
@@ -1,30 +1,18 @@
 import React from "react";
-import { Box, Typography, CardMedia } from "@mui/material";
 
 const WelcomeMessage = () => {
   return (
-    <Box
-      sx={{
-        mt: 4,
-        mb: 4,
-      }}
-    >
-      <Typography variant="body1" sx={{ textAlign: "center" }}>
+    <div className="my-1">
+      <p className="text-center text-lg">
         Use search bar to look for your next amazing adventure!
-      </Typography>
-      <CardMedia
-        component="img"
-        image="/images/welcome_search.webp"
+      </p>
+      <img
+        src="/images/welcome_search.webp"
         alt="Searching"
         title="Illustrated by Sushama Patel, Source: https://dribbble.com/shots/14285317-searching"
-        sx={{
-          margin: "10px auto",
-          maxWidth: 600,
-          width: { sm: "80%", xs: "100%" },
-          height: "auto",
-        }}
+        className="my-2.5 mx-auto max-w-xl w-full h-auto"
       />
-    </Box>
+    </div>
   );
 };
 
