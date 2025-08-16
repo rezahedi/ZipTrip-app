7676c7cd18990713bd8fe7f53c5bcfa2e9d48ac3
Reza Zahedi <r.zahedi@gmail.com>
2025-08-16 13:56:05 -0700

    refactor stops related components

--- src/Components/Common/Stop.tsx
+++ src/Components/Common/Stop.tsx
@@ -1,45 +1,22 @@
 import React from "react";
-import { Card, CardMedia, CardContent, Typography } from "@mui/material";
 import { Stop as StopType } from "@/types";
 
 const Stop = ({ detail }: { detail: StopType }) => {
   const { name, imageURL, address } = detail;
   return (
-    <Card
-      sx={{
-        display: "flex",
-        alignItems: { xs: "flex-start", sm: "center" },
-        gap: 2,
-        flexDirection: { xs: "column", sm: "row" },
-        transition: "box-shadow 0.2s ease",
-        "&:hover": {
-          boxShadow: 6,
-        },
-        "& .MuiCardMedia-root": {
-          transition: "scale 0.2s ease",
-        },
-        "&:hover .MuiCardMedia-root": {
-          scale: 1.05,
-        },
-      }}
-    >
+    <div className="group transition-shadow duration-200 ease-in-out shadow-md hover:shadow-lg rounded-lg flex items-start sm:items-center gap-0.5 flex-col sm:flex-row overflow-hidden bg-foreground/3">
       {imageURL && (
-        <CardMedia
-          component="img"
-          image={imageURL}
+        <img
+          src={imageURL}
           alt={name}
-          sx={{
-            width: { xs: "100%", sm: 220 },
-            borderRadius: "10px",
-            objectFit: "cover",
-          }}
-        ></CardMedia>
+          className="w-full sm:w-56 object-cover group-hover:scale-105 transition-all duration-200"
+        />
       )}
-      <CardContent>
-        <Typography variant="h5">{name}</Typography>
-        <Typography variant="body1">{address}</Typography>
-      </CardContent>
-    </Card>
+      <div className="flex flex-col content-start p-4 gap-2">
+        <h5 className="font-semibold text-lg">{name}</h5>
+        <p>{address}</p>
+      </div>
+    </div>
   );
 };
 
--- src/Components/Common/Stops.tsx
+++ src/Components/Common/Stops.tsx
@@ -1,74 +1,24 @@
 import React from "react";
 import Stop from "./Stop";
-import { Box, Typography } from "@mui/material";
-import {
-  Timeline,
-  TimelineItem,
-  TimelineSeparator,
-  TimelineConnector,
-  TimelineDot,
-  TimelineContent,
-} from "@mui/lab";
-import RoomOutlinedIcon from "@mui/icons-material/RoomOutlined";
 import { Stop as StopType } from "@/types";
+import MarkedBlock from "./MarkedBlock";
 
 const Stops = ({ stops }: { stops: StopType[] }) => {
   return (
-    <Box sx={{ mt: 4, width: { md: 760, xs: "100%" } }}>
-      <Typography variant="h4" sx={{ mb: 2 }}>
-        Activities
-      </Typography>
-      <Timeline
-        position="right"
-        sx={{
-          width: "100%",
-          pl: 0,
-          ".MuiTimelineItem-missingOppositeContent:before": { display: "none" },
-          ".MuiTimelineContent-root": { pt: 1.5, pr: 0, pb: 4, pl: 2 },
-          ".MuiTimelineDot-root": {
-            backgroundColor: "transparent",
-            boxShadow: "none",
-            color: "#4CAF50",
-            p: 0,
-          },
-        }}
-      >
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot>
-              <RoomOutlinedIcon sx={{ width: 30, height: 30 }} />
-            </TimelineDot>
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography variant="h5">Start Point</Typography>
-          </TimelineContent>
-        </TimelineItem>
-        {stops.map((stop, index) => (
-          <TimelineItem key={index}>
-            <TimelineSeparator>
-              <TimelineDot>
-                <RoomOutlinedIcon sx={{ width: 30, height: 30 }} />
-              </TimelineDot>
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>
-              <Stop detail={stop} />
-            </TimelineContent>
-          </TimelineItem>
-        ))}
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot>
-              <RoomOutlinedIcon sx={{ width: 30, height: 30 }} />
-            </TimelineDot>
-          </TimelineSeparator>
-          <TimelineContent>
-            <Typography variant="h5">Finish Point</Typography>
-          </TimelineContent>
-        </TimelineItem>
-      </Timeline>
-    </Box>
+    <div className="mt-4 w-full md:w-3xl">
+      <h4 className="text-4xl mb-2">Activities</h4>
+      <MarkedBlock>
+        <h5 className="text-2xl">Start Point</h5>
+      </MarkedBlock>
+      {stops.map((stop, index) => (
+        <MarkedBlock key={index}>
+          <Stop detail={stop} />
+        </MarkedBlock>
+      ))}
+      <MarkedBlock>
+        <h5 className="text-2xl">Finish Point</h5>
+      </MarkedBlock>
+    </div>
   );
 };
 
