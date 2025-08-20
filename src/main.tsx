e4147fe9bae85148b214f15a43abd8adcb836caa
Reza Zahedi <r.zahedi@gmail.com>
2025-08-19 22:21:15 -0700

    remove MUI theme from app

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "@emotion/react";
-import theme from "./theme";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     // <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>,
-    // {/* </React.StrictMode>, */}
+    <App />,
+    // </React.StrictMode>
   );
 } else {
   throw new Error('Root element with id "root" not found');
