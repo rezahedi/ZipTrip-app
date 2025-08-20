a33826e2189997efad48719f39e32f8d80c9e6b7
Reza Zahedi <r.zahedi@gmail.com>
2025-08-19 17:09:53 -0700

    add resetpass to auth modal

--- src/context/AuthModalContext.tsx
+++ src/context/AuthModalContext.tsx
@@ -3,19 +3,25 @@ import React, { createContext, useContext, useState } from "react";
 type AuthModalContextType = {
   isLoginOpen: boolean;
   isRegisterOpen: boolean;
+  isPasswordResetOpen: boolean;
   openLogin: () => void;
   openRegister: () => void;
+  openPasswordReset: () => void;
   closeLogin: () => void;
   closeRegister: () => void;
+  closePasswordReset: () => void;
 };
 
 const AuthModalContext = createContext<AuthModalContextType>({
   isLoginOpen: false,
   isRegisterOpen: false,
+  isPasswordResetOpen: false,
   openLogin: () => {},
   openRegister: () => {},
+  openPasswordReset: () => {},
   closeLogin: () => {},
   closeRegister: () => {},
+  closePasswordReset: () => {},
 });
 
 export const AuthModalProvider = ({
@@ -25,29 +31,43 @@ export const AuthModalProvider = ({
 }) => {
   const [isLoginOpen, setIsLoginOpen] = useState<boolean>(false);
   const [isRegisterOpen, setIsRegisterOpen] = useState<boolean>(false);
+  const [isPasswordResetOpen, setIsPasswordResetOpen] =
+    useState<boolean>(false);
 
   const openLogin = () => {
     setIsLoginOpen(true);
     setIsRegisterOpen(false);
+    setIsPasswordResetOpen(false);
   };
 
   const openRegister = () => {
     setIsRegisterOpen(true);
     setIsLoginOpen(false);
+    setIsPasswordResetOpen(false);
+  };
+
+  const openPasswordReset = () => {
+    setIsPasswordResetOpen(true);
+    setIsLoginOpen(false);
+    setIsRegisterOpen(false);
   };
 
   const closeLogin = () => setIsLoginOpen(false);
   const closeRegister = () => setIsRegisterOpen(false);
+  const closePasswordReset = () => setIsPasswordResetOpen(false);
 
   return (
     <AuthModalContext.Provider
       value={{
         isLoginOpen,
         isRegisterOpen,
+        isPasswordResetOpen,
         openLogin,
         openRegister,
+        openPasswordReset,
         closeLogin,
         closeRegister,
+        closePasswordReset,
       }}
     >
       {children}
