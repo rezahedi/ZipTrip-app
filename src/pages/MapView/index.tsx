94c006a88d8c6c25abd15617ec4ab1c871cfaea0
Reza Zahedi <r.zahedi@gmail.com>
2025-09-25 21:10:49 -0700

    create bounds change event handler

--- src/pages/MapView/index.tsx
+++ src/pages/MapView/index.tsx
@@ -1,12 +1,14 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import {
   Map,
+  useMap,
   MapCameraChangedEvent,
   MapCameraProps,
   MapControl,
   ControlPosition,
 } from "@vis.gl/react-google-maps";
 import LocateMeButton from "./LocateMeButton";
+import { debounce } from "@/lib/utils";
 
 const INITIAL_CAMERA = {
   center: { lat: 39.8283, lng: -98.5795 },
@@ -14,12 +16,26 @@ const INITIAL_CAMERA = {
 };
 
 const MapViewPage = () => {
+  const map = useMap();
   // TODO: Get user's location from local storage
   const [cameraProps, setCameraProps] =
     useState<MapCameraProps>(INITIAL_CAMERA);
 
-  const handleCameraChange = useCallback(
-    (ev: MapCameraChangedEvent) => setCameraProps(ev.detail),
+  const handleCameraChange = useCallback((ev: MapCameraChangedEvent) => {
+    return setCameraProps(ev.detail);
+  }, []);
+
+  const handleBoundsChange = () => {
+    const bounds = map?.getBounds();
+    // TODO: fetch data and show as pointers on map
+    // TODO: Debounce api calls
+    console.log(
+      `/plans/nearby/?latmin=${bounds?.getSouthWest().lat()}&lngmin=${bounds?.getNorthEast().lng()}&latmax=${bounds?.getNorthEast().lat()}&lngmax=${bounds?.getSouthWest().lng()}`,
+    );
+  };
+
+  const debouncedHandleBoundsChange = useMemo(
+    () => debounce(handleBoundsChange, 300),
     [],
   );
 
@@ -32,6 +48,7 @@ const MapViewPage = () => {
         disableDefaultUI={false}
         gestureHandling="greedy"
         streetViewControl={false}
+        onBoundsChanged={debouncedHandleBoundsChange}
       >
         <MapControl position={ControlPosition.RIGHT_BOTTOM}>
           <LocateMeButton />
