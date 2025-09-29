336956d963ec0435155b6da731367e76c49e6f39
Reza Zahedi <r.zahedi@gmail.com>
2025-09-29 00:47:37 -0700

    return back react strictMode for debugging

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import "./index.css";
 const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
-    // <React.StrictMode>
-    <App />,
-    // </React.StrictMode>
+    <React.StrictMode>
+      <App />,
+    </React.StrictMode>,
   );
 } else {
   throw new Error('Root element with id "root" not found');
