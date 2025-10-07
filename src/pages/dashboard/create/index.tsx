92cbbeba665d430bb77f3f534574cf4c3c9d5fe7
Reza Zahedi <r.zahedi@gmail.com>
2025-10-06 17:42:58 -0700

    remove unused imports

--- src/pages/dashboard/create/index.tsx
+++ src/pages/dashboard/create/index.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import Map from "@/pages/MapView/Map";
 import Sidebar from "./Sidebar";
 import {PlacesProvider} from "@/context/PlacesContext";
-import Markers from "@/pages/MapView/Markers";
-import {ControlPosition, MapControl} from "@vis.gl/react-google-maps";
-import LocateMeButton from "@/pages/MapView/LocateMeButton";
-import PlacePopup from "./PlacePopup";
 import MapBox from "./MapBox";
 
 const CreatePage = () => {
