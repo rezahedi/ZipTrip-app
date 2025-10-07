0350e08cc85dfff1d9de4abfb6c45b8eac2ae96f
Reza Zahedi <r.zahedi@gmail.com>
2025-10-07 16:03:06 -0700

    made title and desc editable

---
+++ src/pages/dashboard/create/Editable.tsx
@@ -0,0 +1,85 @@
+import React, {useState, useEffect, useRef, memo} from "react";
+import {PenIcon} from "lucide-react";
+
+const Editable = function Editable({
+  children,
+  onSave,
+}: {
+  children: string;
+  onSave: (name: string) => void;
+}) {
+  const [message, setMessage] = useState<string>("");
+  const [editable, setEditable] = useState<"false" | "plaintext-only">("false");
+  const [text, setText] = useState(children);
+  const editableRef = useRef<HTMLDivElement>(null);
+
+  // Exit edit mode when focus is lost
+  const handleBlur = (e: React.FocusEvent<HTMLDivElement>) => {
+    if (!editable) return;
+
+    setEditable("false");
+    setMessage("");
+  };
+
+  // Call save action when text changes
+  useEffect(() => {
+    (async () => {
+      if (text === children) return;
+
+      onSave(text);
+      setMessage("Saved!");
+      setTimeout(() => setMessage(""), 2000);
+    })();
+  }, [text]);
+
+  // Edit mode status actions
+  useEffect(() => {
+    if (editable === "plaintext-only")
+      // Focus input if enabled
+      editableRef.current?.focus();
+    else {
+      // Set text state if disabled
+      let newText = editableRef.current!.textContent || "";
+      newText = newText.trim();
+
+      setText(newText);
+    }
+  }, [editable]);
+
+  // Edit mode if clicked
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    setEditable("plaintext-only");
+    setMessage("Autosave, Hit Enter or Esc to exit.");
+    e.currentTarget.focus();
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (["Enter", "Tab", "Escape"].includes(e.key)) {
+      e.preventDefault();
+      e.currentTarget.blur();
+    }
+  };
+
+  return (
+    <span className="select-none inline-flex flex-col">
+      <div className="flex gap-2 items-center">
+        <div
+          ref={editableRef}
+          onBlur={handleBlur}
+          onClick={handleClick}
+          onKeyDown={handleKeyDown}
+          contentEditable={editable}
+          title="Click to inline edit"
+          className={`peer inline-block focus:outline-none focus:border focus:border-gray-300 focus:rounded ${editable === "false" ? `cursor-pointer` : `cursor-text`}`}
+        >
+          {text}
+        </div>
+        <PenIcon className="hidden peer-hover:inline w-4 text-gray-400" />
+      </div>
+      {message && <i className="text-xs text-gray-600">{message}</i>}
+    </span>
+  );
+};
+
+export default Editable;
--- src/pages/dashboard/create/Sidebar.tsx
+++ src/pages/dashboard/create/Sidebar.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ItineraryList from "./ItineraryList";
+import Editable from "./Editable";
 
 const Sidebar = () => {
-  const editMode = false;
+  const handleSave = (text: string) => {
+    console.log("text:", text);
+  };
+
   return (
     <div className="w-lg p-3 h-[calc(100vh-61px)] overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
-      <h2 className="font-semibold text-2xl" contentEditable={editMode}>
-        Your Plan
+      <h2 className="font-semibold text-2xl">
+        <Editable onSave={handleSave}>Your Plan</Editable>
       </h2>
       <p className="text-base mt-2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-        tempora deleniti asperiores atque rerum quibusdam beatae, laudantium
-        corporis voluptatem, consequatur harum sed nemo ab. Blanditiis at
-        doloremque iusto alias nisi.
+        <Editable onSave={handleSave}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+          tempora deleniti asperiores atque rerum quibusdam beatae, laudantium
+          corporis voluptatem, consequatur harum sed nemo ab. Blanditiis at
+          doloremque iusto alias nisi.
+        </Editable>
       </p>
       <h3 className="font-semibold text-xl mt-4">Itinerary</h3>
       <ItineraryList />
