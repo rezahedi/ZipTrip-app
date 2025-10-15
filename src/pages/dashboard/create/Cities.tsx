47b57fab0fbd73907273ceeace45f05feb5ed2bf
Reza Zahedi <r.zahedi@gmail.com>
2025-10-14 17:42:06 -0700

    add city tags to plan detail page

--- src/pages/dashboard/create/Cities.tsx
+++ src/pages/dashboard/create/Cities.tsx
@@ -3,18 +3,40 @@ import { cn } from "@/lib/utils";
 import { CityType } from "@/context/PlanTypes";
 import { XIcon } from "lucide-react";
 import IconButton from "@/Components/ui/IconButton";
+import { Link } from "react-router-dom";
 
 const Cities = ({
   cities,
+  linked = false,
   className = "",
   onRemove,
 }: {
   cities: CityType[];
+  linked?: boolean;
   className?: string;
   onRemove?: (city: CityType) => void;
 }) => {
   if (!cities) return null;
-  console.log("onRemove", onRemove);
+
+  if (linked)
+    return (
+      <>
+        {cities.map((city) => (
+          <Link
+            to={`/city/${city.placeId}`}
+            key={city.placeId}
+            className={cn(
+              "rounded-md text-sm bg-primary/15 text-foreground p-2 px-3 text-nowrap flex flex-row gap-2 items-center",
+              className,
+            )}
+          >
+            <Item city={city} onRemove={onRemove} />
+          </Link>
+        ))}
+      </>
+    );
+
+  // Unlinked items
   return (
     <>
       {cities.map((city) => (
@@ -25,14 +47,28 @@ const Cities = ({
             className,
           )}
         >
-          {city.name}
-          {onRemove && (
-            <IconButton onClick={() => onRemove(city)} className="size-6">
-              <XIcon className="size-9/12" />
-            </IconButton>
-          )}
+          <Item city={city} onRemove={onRemove} />
         </div>
       ))}
+    </>
+  );
+};
+
+const Item = ({
+  city,
+  onRemove,
+}: {
+  city: CityType;
+  onRemove?: (city: CityType) => void;
+}) => {
+  return (
+    <>
+      {city.name}
+      {onRemove && (
+        <IconButton onClick={() => onRemove(city)} className="size-6">
+          <XIcon className="size-9/12" />
+        </IconButton>
+      )}
     </>
   );
 };
