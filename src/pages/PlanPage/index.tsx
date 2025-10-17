bbdeb22abf362c30f9e82806655a9feb3f2136d9
Reza Zahedi <r.zahedi@gmail.com>
2025-10-17 16:55:46 -0700

    add loading skeleton to planDetail pages

--- src/pages/PlanPage/index.tsx
+++ src/pages/PlanPage/index.tsx
@@ -5,20 +5,24 @@ import PlanDetails from "./PlanDetails";
 import { useAuth } from "@/context/AuthContext";
 import { PlanWithStops } from "@/types";
 import CardSection from "@/Components/Common/CardSection";
+import PageSkeleton from "./PageSkeleton";
 
 const PlanPage = () => {
   const [plan, setPlan] = useState<PlanWithStops | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { planId } = useParams();
   const { token } = useAuth();
 
   useEffect(() => {
     const fetchSinglePlan = async (planId: string) => {
+      setIsLoading(true);
       try {
         const res = await fetchData(`plans/${planId}`, token, () => {});
         setPlan(res);
       } catch (error) {
         console.error("Error fetching a single plan:", error);
       }
+      setIsLoading(false);
     };
 
     if (planId) {
@@ -28,7 +32,8 @@ const PlanPage = () => {
 
   return (
     <div className="py-1">
-      {plan ? <PlanDetails plan={plan} /> : <h6>Loading...</h6>}
+      {isLoading && <PageSkeleton />}
+      {!isLoading && plan && <PlanDetails plan={plan} />}
       {plan && plan.cities.length > 0 && (
         <CardSection
           title={`More plans around ${plan.cities[0].name}`}
