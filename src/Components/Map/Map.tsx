6946920ffd7f89b7af748c3ca6fa05e335c60628
Reza Zahedi <r.zahedi@gmail.com>
2025-10-27 17:49:28 -0700

    disable camera control on map

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -56,6 +56,7 @@ const Map = ({
       style={{ width: "100%", height: "100%" }}
       disableDefaultUI={false}
       gestureHandling="greedy"
+      cameraControl={false}
       streetViewControl={false}
       mapTypeControl={false}
       onDragend={debouncedSetBoundingBox}
