3fc5fd785bcd70bde6ada8549ab6d0f45de25c50
Reza Zahedi <r.zahedi@gmail.com>
2025-10-28 21:47:58 -0700

    restructure sidebar divs

--- src/pages/dashboard/create/Sidebar.tsx
+++ src/pages/dashboard/create/Sidebar.tsx
@@ -15,33 +15,35 @@ const Sidebar = () => {
   return (
     <div
       className={cn(
-        "flex-7/12 md:max-w-lg p-3 overflow-y-scroll",
+        "flex-7/12 md:max-w-lg overflow-y-scroll",
         SCROLL_BAR_STYLE,
       )}
     >
-      {saving && <div>Saving ...</div>}
-      {!saving && error && <div>Error: {error}</div>}
-      {plan && (
-        <>
-          <h2 className="font-semibold text-2xl">
-            <Editable onSave={setTitle}>
-              {plan?.title || "Your Next Trip"}
-            </Editable>
-          </h2>
-          <p className="text-base mt-2">
-            <Editable onSave={setDescription}>
-              {plan?.description || "Write something about your trip..."}
-            </Editable>
-          </p>
-          <ImageUploader />
-          <div className="flex gap-2 flex-wrap my-2">
-            <Cities cities={plan.cities || []} />
-          </div>
-          <h3 className="font-semibold text-xl mt-4">Itinerary</h3>
-          <ItineraryList />
-          <AddNewPlace />
-        </>
-      )}
+      <div className="p-3">
+        {saving && <div>Saving ...</div>}
+        {!saving && error && <div>Error: {error}</div>}
+        {plan && (
+          <>
+            <h2 className="font-semibold text-2xl">
+              <Editable onSave={setTitle}>
+                {plan?.title || "Your Next Trip"}
+              </Editable>
+            </h2>
+            <p className="text-base mt-2">
+              <Editable onSave={setDescription}>
+                {plan?.description || "Write something about your trip..."}
+              </Editable>
+            </p>
+            <ImageUploader />
+            <div className="flex gap-2 flex-wrap my-2">
+              <Cities cities={plan.cities || []} />
+            </div>
+            <h3 className="font-semibold text-xl mt-4">Itinerary</h3>
+            <ItineraryList />
+          </>
+        )}
+      </div>
+      <AddNewPlace />
     </div>
   );
 };
