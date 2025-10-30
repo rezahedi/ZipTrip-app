d3120564aaa4d3d1f58ded821e72efa9a01c17ac
Reza Zahedi <r.zahedi@gmail.com>
2025-10-30 09:24:16 -0700

    show two skeleton cards in mobile screens

--- src/pages/ExplorePage/places/list/PlaceCardSkeleton.tsx
+++ src/pages/ExplorePage/places/list/PlaceCardSkeleton.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Skeleton } from "@/Components/ui/skeleton";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 const PlaceCardSkeleton = () => {
+  const { isMobile } = useMediaQuery();
+  const length = isMobile ? 2 : 3;
+
   return (
     <>
-      {Array.from({ length: 3 }).map((v, i) => (
+      {Array.from({ length }).map((v, i) => (
         <Skeleton
           key={i}
           className="flex gap-1 aspect-[16/10] sm:aspect-[6/5] relative cursor-pointer"
