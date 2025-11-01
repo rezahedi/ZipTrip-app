c5f9cfa197b526a3db77e95b9f9ab0244ccd2cf9
Reza Zahedi <r.zahedi@gmail.com>
2025-11-01 14:15:41 -0700

    remove notif bar as switched to vercel for api server

--- src/Components/Layout/MainLayout.tsx
+++ src/Components/Layout/MainLayout.tsx
@@ -2,14 +2,12 @@ import React, { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
-import NotificationBar from "@/pages/HomePage/NotificationBar";
 
 export default function MainLayout() {
   return (
     <>
       <ScrollToTop />
       <div className="relative mx-auto px-2 sm:px-3 w-full max-w-[1300px] box-border">
-        <NotificationBar />
         <Header />
       </div>
 
--- src/Components/Layout/MapLayout.tsx
+++ src/Components/Layout/MapLayout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "@/Components/Header";
-import NotificationBar from "@/pages/HomePage/NotificationBar";
 import { APIProvider } from "@vis.gl/react-google-maps";
 
 const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
@@ -15,7 +14,6 @@ function MapLayout() {
   return (
     <div className="flex flex-col h-screen">
       <div className="relative mx-auto px-2 sm:px-3 w-full max-w-[1300px] box-border">
-        <NotificationBar />
         <Header withBanner={false} />
       </div>
 
