ee536e2bae0682d830adf5f693734e595831bf7e
Reza Zahedi <r.zahedi@gmail.com>
2025-11-01 13:56:24 -0700

    remove api proxy from vite config

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
