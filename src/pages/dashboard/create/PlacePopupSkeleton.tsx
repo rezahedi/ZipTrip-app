0359344eeb0187c435640476233f3dc20635f6f8
Reza Zahedi <r.zahedi@gmail.com>
2025-11-03 16:15:45 -0800

    fix popup skeleton to light theme always

--- src/pages/dashboard/create/PlacePopupSkeleton.tsx
+++ src/pages/dashboard/create/PlacePopupSkeleton.tsx
@@ -4,11 +4,11 @@ import { Skeleton } from "@/Components/ui/skeleton";
 const PlacePopupSkeleton = () => {
   return (
     <div className="flex gap-1 w-[230px] h-[128px]">
-      <Skeleton className="w-24 h-full rounded-sm hidden sm:block" />
+      <Skeleton className="w-24 h-full rounded-sm hidden sm:block dark:bg-background/10" />
       <div className="flex-4/5 max-h-40 px-2">
-        <Skeleton className="w-9/12 h-7 mb-4" />
-        <Skeleton className="h-4 mb-2" />
-        <Skeleton className="h-4 w-8/12 mb-2" />
+        <Skeleton className="w-9/12 h-7 mb-4 dark:bg-background/10" />
+        <Skeleton className="h-4 mb-2 dark:bg-background/10" />
+        <Skeleton className="h-4 w-8/12 mb-2 dark:bg-background/10" />
       </div>
     </div>
   );
