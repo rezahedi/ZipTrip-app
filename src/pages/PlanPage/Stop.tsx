b4c17515962362f29251ebd7521f82373ed82549
Reza Zahedi <r.zahedi@gmail.com>
2025-11-05 14:30:13 -0800

    show review on each stop in plan detail page

--- src/pages/PlanPage/Stop.tsx
+++ src/pages/PlanPage/Stop.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import { Place } from "@/types";
 import { getMarkerIcon } from "@/types/map";
 import { useMapSync } from "@/context/MapSyncContext";
-import { cn } from "@/lib/utils";
-import { CirclePlusIcon, MapPinnedIcon, RouteIcon } from "lucide-react";
+import { cn, formatNumber } from "@/lib/utils";
+import {
+  CirclePlusIcon,
+  MapPinnedIcon,
+  RouteIcon,
+  StarIcon,
+} from "lucide-react";
 import { useList } from "@/context/ListContext";
 import { Button } from "@/Components/ui/button";
 import { Link } from "react-router-dom";
@@ -11,19 +16,6 @@ import { useAuth } from "@/context/AuthContext";
 import { useAuthModal } from "@/context/AuthModalContext";
 
 const Stop = ({ place }: { place: Place }) => {
-  const {
-    placeId,
-    name,
-    address,
-    imageURL,
-    rating,
-    userRatingCount,
-    placeGoogleURI,
-    directionGoogleURI,
-    type,
-    reviewSummary,
-    summary,
-  } = place;
   const { selection, setSelection } = useMapSync();
   const { openEditor } = useList();
   const { token } = useAuth();
@@ -32,14 +24,14 @@ const Stop = ({ place }: { place: Place }) => {
   const handleOpenEditor = () => {
     if (!token) return openLogin();
 
-    openEditor(place as Place);
+    openEditor(place);
   };
 
   const handleSetSelection = () => {
     setSelection({ placeId: place.placeId, source: "card" });
   };
 
-  if (!placeId) return null;
+  if (!place.placeId) return null;
 
   return (
     <div
@@ -47,39 +39,44 @@ const Stop = ({ place }: { place: Place }) => {
       onMouseOver={handleSetSelection}
       className={cn(
         `group transition-shadow duration-200 ease-in-out shadow-md hover:shadow-lg rounded-lg flex items-start sm:items-center gap-0.5 flex-col sm:flex-row overflow-hidden bg-foreground/3`,
-        selection && selection.placeId === placeId ? `bg-foreground/10` : ``,
+        selection && selection.placeId === place.placeId
+          ? `bg-foreground/10`
+          : ``,
       )}
     >
-      {imageURL && (
+      {place.imageURL && (
         <img
-          src={imageURL}
-          alt={name}
+          src={place.imageURL}
+          alt={place.name}
           className="w-full sm:w-56 object-cover group-hover:scale-105 transition-all duration-200"
         />
       )}
       <div className="flex flex-col items-start py-2 p-4 gap-2">
         <h5 className="font-semibold text-lg">
-          {getMarkerIcon(type)} {name}
+          {getMarkerIcon(place.type)} {place.name}
         </h5>
-        <p>{address}</p>
-        {rating && (
-          <p className="line-clamp-2 text-sm">
-            {rating} / {userRatingCount} reviews
-          </p>
-        )}
-        {(summary || reviewSummary) && (
-          <p className="text-sm">{summary || reviewSummary}</p>
+        <p>{place.address}</p>
+        {place.summary && <p className="text-sm">{place.summary}</p>}
+        {place.rating && (
+          <div className="text-sm">
+            <p className="flex gap-1 items-center text-sm">
+              <StarIcon className="size-3" />
+              <b className="font-semibold">{place.rating}</b> (
+              {formatNumber(place.userRatingCount)} reviews)
+            </p>
+            {place.reviewSummary || ""}
+          </div>
         )}
         <div className="flex flex-wrap items-center gap-1 mt-4 lg:opacity-0 group-hover:opacity-100">
-          {placeGoogleURI && (
-            <Link to={placeGoogleURI} target="_blank">
+          {place.placeGoogleURI && (
+            <Link to={place.placeGoogleURI} target="_blank">
               <Button variant="outline" className="hover:bg-foreground/20">
                 <MapPinnedIcon /> Google Map
               </Button>
             </Link>
           )}
-          {directionGoogleURI && (
-            <Link to={directionGoogleURI} target="_blank">
+          {place.directionGoogleURI && (
+            <Link to={place.directionGoogleURI} target="_blank">
               <Button variant="outline" className="hover:bg-foreground/20">
                 <RouteIcon /> Direction
               </Button>
