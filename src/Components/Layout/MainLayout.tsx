ab8be767e80fc362db813fdfa8013e59dbf1f321
Reza Zahedi <r.zahedi@gmail.com>
2025-11-08 09:22:44 -0800

    move reactoauth provider to layout

--- src/Components/Layout/MainLayout.tsx
+++ src/Components/Layout/MainLayout.tsx
@@ -2,13 +2,18 @@ import React, { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function MainLayout() {
   return (
     <>
       <ScrollToTop />
       <div className="relative mx-auto px-2 sm:px-3 w-full max-w-[1300px] box-border">
-        <Header />
+        <GoogleOAuthProvider
+          clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}
+        >
+          <Header />
+        </GoogleOAuthProvider>
       </div>
 
       <div className="my-5 mx-auto px-2 sm:px-3 w-full max-w-[1300px] box-border">
